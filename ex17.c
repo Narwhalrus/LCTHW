00d16ecea07ef0d8de9fb02327c1edceb92f47b9
Narwhalrus <j.p.riccobono2@gmail.com>
2013-12-07 22:53:45 -0600

    More...

--- ex17.c
+++ ex17.c
@@ -49,3 +49,63 @@ void Database_load(struct Connection *conn)
 }
 
 struct Connection *Database_open(const char *filename, char mode)
+{
+	struct Connection *conn = malloc(sizeof(struct Connection));
+	if(!conn) die("Memory error");
+
+	conn->db = malloc(sizeof(struct Database));
+	if(!conn->db) die("Memory error");
+
+	if(mode == 'c') {
+		conn->file = fopen(filename, "w");
+	} else {
+		conn->file = fopen(filename, "r+");
+		
+		if(conn->file) {
+			Database_load(conn);
+		}
+	}
+
+	if(!conn->file) die("Failed to open the file");
+
+	return conn;
+}
+
+void Database_close(struct Connection *conn)
+{
+	if(conn) {
+		if(conn->file) fclose(conn->file);
+		if(conn->db) free(conn->db);
+		free(conn);
+	}
+}
+
+void Database_create(struct Connection *conn)
+{
+	int i = 0;
+	
+	for(i = 0; i < MAX_ROWS; i++) {
+		// make a prototype to initialize it
+		struct Address addr = {.id = i, .set = 0};
+		// then just assign it
+		conn->db->rows[i] = addr;
+	}
+}
+
+void Database_set(struct Connection *conn, int id)
+{
+	struct Address *addr = &conn->db->rows[id];
+
+	if(addr->set) die("Already set, delete it first");
+	
+	addr->set = 1;
+	// WARNING: bug, read the "How To Break It" and fix this
+	char *res = strncpy(addr->name, name, MAX_DATA);
+	// demonstrate the strncpy bug
+	if(!res) die("Name copy failed");
+
+	res = strncpy(addr->email, email, MAX_DATA);
+	if(!res) die("Email copy failed");
+}
+
+
