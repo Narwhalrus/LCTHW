e870b57766cc9042d5eb618723247a64f91247b1
Narwhalrus <j.p.riccobono2@gmail.com>
2013-12-20 21:19:23 -0600

    eh

---
+++ ctypes_test/lib_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "lib_test.h"
+void *create_memory_space(void)
+{
+	int i;
+	int *buffer = NULL;
+
+	// Allocate block of memory to be used in python
+	buffer = malloc(sizeof(int) * 200);
+	if(!buffer) {
+		puts("OH SHIT!!");
+		return NULL;
+	}	
+
+	for(i = 0; i < 200; i++) {
+		buffer[i] = i;
+	}
+
+	return (void *)buffer;
+}
+
+void destroy_memory_space(void *buffer) {
+	if(buffer) {
+		free(buffer);
+	}
+}
---
+++ ctypes_test/lib_test.h
@@ -0,0 +1,13 @@
+#ifndef _LIB_TEST_H_
+#define _LIB_TEST_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *create_memory_space(void);
+void destroy_memory_space(void *buffer);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _LIB_TEST_H_ */
